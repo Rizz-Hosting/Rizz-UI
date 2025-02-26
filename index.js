0515e499f18d25570ae8a8375e97d16eb2fe48b8
RizzHosting <rizzha62@gmail.com>
2025-02-26 13:43:47 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const {
   generateQRIS,
   createQRIS,
   checkQRISStatus
-} = require('./function/orkut.js')
+} = require('./src/api/orkut/orkut.js')
 
 const app = express();
 const PORT = process.env.PORT || 4000;
